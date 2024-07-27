0e7f8d65a10a0c0b028c495b33470de0c084bb32
minhajul9 <110691447+minhajul9@users.noreply.github.com>
2024-07-27 20:37:56 +0600

    user signup

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,11 +1,49 @@
-export const signup = (req, res) =>{
-    console.log("signup user");
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req, res) => {
+    try {
+        const { fullName, username, gender, password } = req.body;
+
+        const user = await User.findOne({ username })
+
+        if (user) {
+            return res.status(400).json({ error: "Username already exist!" })
+        }
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        const boyAvatar = `https://avatar.iran.liara.run/public/boy?username=${username}`
+        const girlAvatar = `https://avatar.iran.liara.run/public/girl?username=${username}`
+
+        const newUser = new User({
+            fullName,
+            username,
+            gender,
+            password: hashedPassword,
+            profilePic: gender === 'male' ? boyAvatar : girlAvatar
+        })
+
+        await newUser.save();
+
+        res.status(201).json({
+            _id: newUser._id,
+            fullName: newUser.fullName,
+            username: newUser.username,
+            gender: newUser.gender,
+            profilePic: newUser.profilePic
+        })
+
+    } catch (error) {
+        console.log("error in signup controller", error.message);
+        res.status(500).json({ error: "Internal server error" })
+    }
 }
 
-export const login = (req, res) =>{
+export const login = (req, res) => {
     console.log("login user");
 }
 
-export const logout = (req, res) =>{
+export const logout = (req, res) => {
     console.log('log out');
 }
--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 dotenv.config();
 
+app.use(express.json());
+
 
 app.get('/', (req, res) =>{
     res.send('chatting app backend')
