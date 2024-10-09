5e53d7332252d1ebd2a54f83050b6c7394a75636
Minhaj Ul Islam Chowdhury <mulislamchowdhury@gmail.com>
2024-10-09 12:47:58 +0600

    Create user.controller.js

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,15 @@
+import User from "../models/user.model.js";
+
+export const getUsersForSidebar = async (req, res) => {
+    try {
+
+        const loggedInUser = req.user._id;
+
+        const filteredUsers = await User.find({ _id: { $ne: loggedInUser } });
+
+        res.status(200).json(filteredUsers);
+    } catch (error) {
+        console.log("error in signup controller", error.message);
+        res.status(500).json({ error: "Internal server error" })
+    }
+}
