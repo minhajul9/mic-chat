5ce5d9f6ac97be4463f5749f3a2d870cdb1fcbc7
Minhaj Ul Islam Chowdhury <mulislamchowdhury@gmail.com>
2024-10-09 12:51:50 +0600

    remove password of the users

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -5,7 +5,7 @@ export const getUsersForSidebar = async (req, res) => {
 
         const loggedInUser = req.user._id;
 
-        const filteredUsers = await User.find({ _id: { $ne: loggedInUser } });
+        const filteredUsers = await User.find({ _id: { $ne: loggedInUser } }).select('-password');
 
         res.status(200).json(filteredUsers);
     } catch (error) {
