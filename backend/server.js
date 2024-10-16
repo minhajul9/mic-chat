44f2110e08cfc6d42af5ffbbdd8dcf3f81d06d4f
minhajul9 <110691447+minhajul9@users.noreply.github.com>
2024-10-16 20:52:50 +0600

    setup

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ import userRoutes from './routes/user.routes.js'
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5004;
 dotenv.config();
 
 app.use(express.json());
