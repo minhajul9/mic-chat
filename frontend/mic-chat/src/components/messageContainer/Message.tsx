a895dcde915dfcd63924c218aa6a028e4f7875bd
minhaj <mulislamchowdhury@gmail.com>
2024-11-04 15:45:42 +0600

    messing complete

--- frontend/mic-chat/src/components/messageContainer/Message.tsx
+++ frontend/mic-chat/src/components/messageContainer/Message.tsx
@@ -1,11 +1,13 @@
 import { useAuthContext } from "../../context/AuthContext"
 import useConversation from "../../zustand/useConversation";
+import { extractTime } from "../utils/extractTime";
 
 const Message = ({ message }) => {
 
     const { authUser } = useAuthContext();
     const { selectedConversation } = useConversation();
     const fromMe = message.senderId === authUser._id;
+    const formattedTime = extractTime(message.createdAt)
 
     return (
         <div className={`chat ${fromMe ? "chat-end" : "chat-start"}`}>
@@ -17,11 +19,11 @@ const Message = ({ message }) => {
                 </div>
             </div>
             <div className="chat-header">
-                Anakin
-                <time className="text-xs opacity-50">12:46</time>
+                {/* Anakin */}
+                <time className="text-xs opacity-50"> {formattedTime}</time>
             </div>
             <div className={`chat-bubble text-white ${fromMe ? 'bg-blue-500' : ''}`}>{message.message}</div>
-            <div className="chat-footer opacity-50">Seen at 12:46</div>
+            <div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>{formattedTime}</div>
         </div>
     )
 }
--- frontend/mic-chat/src/components/messageContainer/Messages.tsx
+++ frontend/mic-chat/src/components/messageContainer/Messages.tsx
@@ -1,14 +1,30 @@
+import { useEffect, useRef } from "react";
 import useGetMessages from "../../hooks/useGetMessages"
 import MessageSkeleton from "../skeletons/MessageSkeleton";
 import Message from "./Message"
+import useConversation from "../../zustand/useConversation";
 
 const Messages = () => {
   const { messages, loading } = useGetMessages();
-  console.log("messages", messages)
+  const { selectedConversation } = useConversation();
+  const lastMessageRef = useRef<HTMLDivElement | null>(null);
+
+
+  useEffect(() => {
+    setTimeout(() => {
+      lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, 500)
+  }, [selectedConversation, messages])
+
   return (
     <div className='px-4 flex-1 overflow-auto'>
-      {!loading && messages.length > 0 && messages.map((message) =>(
-        <Message key={message._id} message={message} />
+      {!loading && messages.length > 0 && messages.map((message) => (
+        <div
+          key={message._id}
+          ref={lastMessageRef}
+        >
+          <Message message={message} />
+        </div>
       ))}
 
       {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
