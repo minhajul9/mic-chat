d1269fa05edeb9c8d013b8f7b7531d6d85bfeea5
minhaj <mulislamchowdhury@gmail.com>
2024-11-18 12:43:48 +0600

    register page design

--- android-app/chat_app/lib/src/pages/registerPage/registration_page.dart
+++ android-app/chat_app/lib/src/pages/registerPage/registration_page.dart
@@ -1,4 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:chat_app/backendConfig/config.dart';
+import 'package:chat_app/src/pages/homePage/home_page.dart';
+import 'package:chat_app/src/pages/loginPage/login_page.dart';
+import 'package:chat_app/src/providers/auth_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class RegistrationPage extends StatefulWidget {
   const RegistrationPage({super.key});
@@ -8,8 +15,275 @@ class RegistrationPage extends StatefulWidget {
 }
 
 class _RegistrationPageState extends State<RegistrationPage> {
+  //
+  final _formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final nameController = TextEditingController();
+  final passwordController = TextEditingController();
+  final confirmPasswordController = TextEditingController();
+  String baseUrl = Config.baseUrl;
+  bool showPassword = false;
+  bool showConfirmPassword = false;
+  bool isLoading = false;
+  String? gender;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        backgroundColor: Color.fromARGB(255, 7, 15, 43),
+        body: Center(
+          child: SingleChildScrollView(
+            child: Container(
+              constraints: BoxConstraints(maxWidth: 660),
+              width: MediaQuery.of(context).size.width * 0.8,
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 146, 144, 195),
+                  borderRadius: BorderRadius.circular(10)),
+              child: Form(
+                  key: _formKey,
+                  child: Consumer<AuthProvider>(
+                      builder: (context, authProvider, child) => Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              //
+                              //logo
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(15),
+                                child: Image(
+                                  image: AssetImage('assets/images/logo.png'),
+                                  width: 200,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 15,
+                              ),
+
+                              //
+                              //username
+                              TextFormField(
+                                controller: emailController,
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return "Please enter Full name.";
+                                  }
+                                  return null;
+                                },
+                                decoration: const InputDecoration(
+                                  filled: true,
+                                  fillColor: Colors.white,
+                                  hintText: "Enter your Full name",
+                                  prefixIcon: Icon(Icons.perm_identity),
+                                  border: OutlineInputBorder(),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 15,
+                              ),
+
+                              //username
+                              TextFormField(
+                                controller: emailController,
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return "Please enter username.";
+                                  }
+                                  return null;
+                                },
+                                decoration: const InputDecoration(
+                                  filled: true,
+                                  fillColor: Colors.white,
+                                  hintText: "Enter your username",
+                                  prefixIcon: Icon(Icons.perm_identity),
+                                  border: OutlineInputBorder(),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 15,
+                              ),
+
+                              //password
+                              TextFormField(
+                                controller: passwordController,
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return "Please enter your password.";
+                                  }
+                                  return null;
+                                },
+                                obscureText: !showPassword,
+                                decoration: InputDecoration(
+                                    filled: true,
+                                    fillColor: Colors.white,
+                                    hintText: "Enter your password",
+                                    prefixIcon: Icon(Icons.lock_outline),
+                                    border: OutlineInputBorder(),
+                                    suffixIcon: IconButton(
+                                      icon: Icon(showPassword
+                                          ? Icons.visibility_off
+                                          : Icons.visibility),
+                                      onPressed: () {
+                                        setState(() {
+                                          showPassword = !showPassword;
+                                        });
+                                      },
+                                    )),
+                              ),
+                              SizedBox(
+                                height: 15,
+                              ),
+
+                              //confirm password
+                              TextFormField(
+                                controller: confirmPasswordController,
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return "Please enter your password.";
+                                  } else if (value != passwordController.text) {
+                                    return "Password did not matched.";
+                                  }
+                                  return null;
+                                },
+                                obscureText: !showConfirmPassword,
+                                decoration: InputDecoration(
+                                    filled: true,
+                                    fillColor: Colors.white,
+                                    hintText: "Confirm your password",
+                                    prefixIcon: Icon(Icons.lock_outline),
+                                    border: OutlineInputBorder(),
+                                    suffixIcon: IconButton(
+                                      icon: Icon(showConfirmPassword
+                                          ? Icons.visibility_off
+                                          : Icons.visibility),
+                                      onPressed: () {
+                                        setState(() {
+                                          showConfirmPassword =
+                                              !showConfirmPassword;
+                                        });
+                                      },
+                                    )),
+                              ),
+                              SizedBox(
+                                height: 15,
+                              ),
+
+                              //gender
+                              Container(
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(5)),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    //male
+                                    Flexible(
+                                      child: CheckboxListTile(
+                                        value: gender == 'male',
+                                        onChanged: (value) {
+                                          setState(() {
+                                            gender = 'male';
+                                          });
+                                        },
+                                        title: Text("Male"),
+                                        controlAffinity:
+                                            ListTileControlAffinity.leading,
+                                        dense: true,
+                                        contentPadding: EdgeInsets.all(0),
+                                      ),
+                                    ),
+
+                                    //female
+                                    Flexible(
+                                      child: CheckboxListTile(
+                                        value: gender == 'female',
+                                        onChanged: (value) {
+                                          setState(() {
+                                            gender = 'female';
+                                          });
+                                        },
+                                        title: Text("Female"),
+                                        controlAffinity:
+                                            ListTileControlAffinity.leading,
+                                        dense: true,
+                                        contentPadding: EdgeInsets.all(0),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              SizedBox(
+                                height: 15,
+                              ),
+
+                              //
+                              ElevatedButton(
+                                onPressed: () async {
+                                  setState(() {
+                                    isLoading = true;
+                                  });
+
+                                  if (_formKey.currentState!.validate()) {
+                                    bool status = await authProvider
+                                        .handleLogin({
+                                      'email': emailController.text,
+                                      'password': passwordController.text
+                                    });
+
+                                    if (status) {
+                                      Navigator.pushReplacement(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) => HomePage()),
+                                      );
+                                    } else {
+                                      setState(() {
+                                        isLoading = false;
+                                      });
+                                    }
+                                  } else {
+                                    setState(() {
+                                      isLoading = false;
+                                    });
+                                  }
+                                },
+                                style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.blue),
+                                child: isLoading
+                                    ? CircularProgressIndicator()
+                                    : Text("Register",
+                                        style: TextStyle(color: Colors.white)),
+                              ),
+                              SizedBox(
+                                height: 20,
+                              ),
+
+                              //link to register page
+                              InkWell(
+                                  onTap: () {
+                                    Navigator.pushReplacement(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              LoginPage()),
+                                    );
+                                  },
+                                  child: Row(
+                                    children: const [
+                                      Text("Already have an account?"),
+                                      SizedBox(
+                                        width: 2,
+                                      ),
+                                      Text(
+                                        "Login",
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ],
+                                  )),
+                            ],
+                          ))),
+            ),
+          ),
+        ));
   }
 }
