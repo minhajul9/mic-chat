2e0000929323cb6db54db4e9311e56126db864be
minhaj <mulislamchowdhury@gmail.com>
2024-11-18 14:25:04 +0600

    homepage design

--- android-app/chat_app/lib/src/pages/homePage/home_page.dart
+++ android-app/chat_app/lib/src/pages/homePage/home_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,6 +12,46 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 78,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Wrap(
+              crossAxisAlignment: WrapCrossAlignment.center,
+              children: [
+                IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.menu,
+                      size: 35,
+                      color: Colors.white,
+                    )),
+                SizedBox(
+                  width: 10,
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(5),
+                  child: Image(
+                    image: AssetImage('assets/images/logo.png'),
+                    width: 70,
+                  ),
+                ),
+              ],
+            ),
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.search,
+                  size: 35,
+                  color: Colors.white,
+                ))
+          ],
+        ),
+        backgroundColor: Color.fromARGB(255, 146, 144, 195),
+      ),
+      backgroundColor: Color.fromARGB(255, 7, 15, 43),
+    );
   }
 }
